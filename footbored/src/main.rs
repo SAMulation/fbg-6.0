ffd4c8e1fd1bfd1a9ecd84ebc74b40b88084d2a6
Sam Sarsten <sam.sarsten@gmail.com>
2023-05-19 22:38:09 -0700

    Updated IP address

--- footbored/src/main.rs
+++ footbored/src/main.rs
@@ -7,7 +7,7 @@ use websocket::index;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
