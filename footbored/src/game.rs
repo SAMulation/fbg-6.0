fc61b5ddb511f1527231dbb4f509f2a9066aacf7
Sam Sarsten <sam.sarsten@gmail.com>
2023-05-20 00:59:31 -0700

    removing comments in game
    adding comment to main

--- footbored/src/game.rs
+++ footbored/src/game.rs
@@ -47,35 +47,6 @@ impl Game {
         true
     }
 
-    // fn is_finished(&self) -> Option<Cell> {
-    //     // Check rows and columns
-    //     for i in 0..SIZE {
-    //         if self.board[i][0] == self.board[i][1] && self.board[i][1] == self.board[i][2] && self.board[i][0] != Cell::Empty {
-    //             return Some(self.board[i][0].clone());
-    //         }
-    //         if self.board[0][i] == self.board[1][i] && self.board[1][i] == self.board[2][i] && self.board[0][i] != Cell::Empty {
-    //             return Some(self.board[0][i].clone());
-    //         }
-    //     }
-    //     // Check diagonals
-    //     if self.board[0][0] == self.board[1][1] && self.board[1][1] == self.board[2][2] && self.board[0][0] != Cell::Empty {
-    //         return Some(self.board[0][0].clone());
-    //     }
-    //     if self.board[0][2] == self.board[1][1] && self.board[1][1] == self.board[2][0] && self.board[0][2] != Cell::Empty {
-    //         return Some(self.board[0][2].clone());
-    //     }
-    //     // Check if there is any Empty cell left
-    //     for i in 0..SIZE {
-    //         for j in 0..SIZE {
-    //             if self.board[i][j] == Cell::Empty {
-    //                 return None;
-    //             }
-    //         }
-    //     }
-    //     // If all cells are full and no one has won, it's a draw
-    //     Some(Cell::Empty)
-    // }
-
     // method to create a game state string that can be sent to the client
     pub fn to_string(&self) -> String {
         let mut state_string = String::new();
@@ -110,28 +81,4 @@ impl fmt::Display for Game {
         Ok(())
     }
 }
-    
-    // fn main() {
-    //     let mut game = Game::new();
-    //     while let None = game.is_finished() {
-    //         println!("{}", game);
-    //         let mut input = String::new();
-    //         io::stdin().read_line(&mut input).expect("Failed to read line");
-    //         let coords: Vec<usize> = input.trim().split(',').map(|x| x.trim().parse().expect("Invalid input")).collect();
-    //         if coords.len() != 2 {
-    //             println!("Please enter coordinates in the format x,y");
-    //             continue;
-    //         }
-    //         if !game.play(coords[0], coords[1]) {
-    //             println!("Invalid move");
-    //             continue;
-    //         }
-    //     }
-    //     println!("{}", game);
-    //     match game.is_finished() {
-    //         Some(Cell::X) => println!("Player X wins!"),
-    //         Some(Cell::O) => println!("Player O wins!"),
-    //         _ => println!("It's a draw!"),
-    //     }
-    // }
     
--- footbored/src/main.rs
+++ footbored/src/main.rs
@@ -6,8 +6,11 @@ use websocket::index;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let address = "0.0.0.0:8080";
+    println!("Starting Rust server on {}", address);
+
     HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
-        .bind("0.0.0.0:8080")?
+        .bind(address)?
         .run()
         .await
 }
