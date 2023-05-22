24dc8c1f78f487235f0047529b6dc3332fe8abbb
Sam Sarsten <sam.sarsten@gmail.com>
2023-05-21 20:34:35 -0700

    trying lobby again

--- footbored/src/game_server.rs
+++ footbored/src/game_server.rs
@@ -3,8 +3,9 @@ use std::collections::HashMap;
 use warp::Filter;
 use tokio::sync::Mutex;
 use log::info;
-use crate::websocket::{handle_connection};
+use crate::websocket::handle_connection;
 use crate::lobby::start_lobby;
+use std::sync::atomic::AtomicBool;
 
 pub async fn start_server() {
     // Create a shared state of connected clients
@@ -13,15 +14,19 @@ pub async fn start_server() {
     // Start the lobby
     let lobby = start_lobby().await;
 
+    // Create a flag to indicate if the server is running
+    let running = Arc::new(AtomicBool::new(true));
+
     // Define WebSocket routes
     let routes = warp::path("ws")
         .and(warp::ws())
         .map(move |ws: warp::ws::Ws| {
             let clients = clients.clone();
             let lobby = lobby.clone();
+            let running = running.clone();
             // Upgrade HTTP connection to WebSocket and handle it
             ws.on_upgrade(move |websocket| {
-                handle_connection(websocket, clients.clone(), lobby.clone())
+                handle_connection(websocket, clients.clone(), lobby.clone(), running.clone())
             })
         });
 
--- footbored/src/websocket.rs
+++ footbored/src/websocket.rs
@@ -1,73 +1,125 @@
 use std::sync::Arc;
 use std::collections::HashMap;
-use tokio::{sync::{Mutex, broadcast}, spawn};
+use tokio::sync::{Mutex, broadcast};
+use tokio::task::spawn;
 use uuid::Uuid;
 use log::{info, warn, error};
 use warp::ws::{Message, WebSocket};
-use futures::{StreamExt, SinkExt};
-use crate::lobby::Lobby;
+use futures::{SinkExt, StreamExt};
+use crate::lobby::{Lobby, Player};
+use futures::stream::SplitSink;
+use futures::stream::SplitStream;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 pub async fn handle_connection(
     ws: WebSocket,
     clients: Arc<Mutex<HashMap<Uuid, broadcast::Sender<String>>>>,
     lobby: Arc<Mutex<Lobby>>,
-    ) {
-        // Split WebSocket into sender and receiver halves
-        let (mut client_ws_tx, mut client_ws_rx) = ws.split();
-        // Create a broadcast channel for incoming messages
-        let (client_tx, _) = broadcast::channel::<String>(10); // Set an appropriate buffer size
-        // Generate a unique ID for each client
-        let client_id = Uuid::new_v4();
-        // Register the client by inserting the sender half into the shared state
-        clients.lock().await.insert(client_id, client_tx.clone());
+    running: Arc<AtomicBool>,
+) {
+    let (client_id, client_ws_tx, client_ws_rx) = initialize_connection(ws, clients.clone()).await;
 
-        // Add logic to handle lobby-related messages
-        // Add logic to handle lobby-related messages
-        while let Some(result) = client_ws_rx.next().await {
-            match result {
-                Ok(message) => {
-                    // Handle lobby-related messages, such as game requests and lobby chat messages
-                    if let Ok(text) = message.to_str() {
-                        if text.starts_with("/message") {
-                            // Extract relevant data from the message and broadcast it to all players
-                            let chat_message = text.to_string();
-                            lobby.lock().await.broadcast_message(chat_message, &clients);
-                        } else if text.starts_with("/request_game") {
-                            // Placeholder for requesting a game
-                            // Extract relevant data from the message and create a game request
-                            // let request = create_game_request_from_message(text);
-                            // lobby.lock().await.send_game_request(request);
-                        } else if text.starts_with("/leave_lobby") {
-                            // Placeholder for leaving the lobby gracefully
-                            // Extract relevant data from the message, such as player ID
-                            // lobby.lock().await.remove_player(player_id);
-                        }
+    let handle_message_task = spawn(handle_message(client_id, client_ws_rx, Arc::clone(&clients), Arc::new(Mutex::new(client_ws_tx)), Arc::clone(&lobby), Arc::clone(&running)));
+    let handle_send_task = spawn(handle_send(client_id, Arc::clone(&clients)));
+
+    tokio::select! {
+        _ = handle_message_task => (),
+        _ = handle_send_task => (),
+    }
+
+    cleanup_connection(client_id, clients, running).await;
+}
+
+async fn initialize_connection(
+    ws: WebSocket,
+    clients: Arc<Mutex<HashMap<Uuid, broadcast::Sender<String>>>>,
+) -> (Uuid, SplitSink<WebSocket, Message>, SplitStream<WebSocket>) {
+    let (client_ws_tx, client_ws_rx) = ws.split();
+    let client_id = Uuid::new_v4();
+    clients.lock().await.insert(client_id, broadcast::channel::<String>(10).0);
+    (client_id, client_ws_tx, client_ws_rx)
+}
+
+async fn handle_message(
+    client_id: Uuid,
+    mut client_ws_rx: SplitStream<WebSocket>,
+    clients: Arc<Mutex<HashMap<Uuid, broadcast::Sender<String>>>>,
+    client_ws_tx: Arc<Mutex<SplitSink<WebSocket, Message>>>,
+    lobby: Arc<Mutex<Lobby>>,
+    running: Arc<AtomicBool>,
+) {
+    while let Some(result) = client_ws_rx.next().await {
+        match result {
+            Ok(message) => {
+                if let Ok(text) = message.to_str() {
+                    if text.starts_with("/message") {
+                        let chat_message = text.to_string();
+                        lobby.lock().await.broadcast_message(chat_message, &clients).await;
+                    } else if text.starts_with("/request_game") {
+                        // Placeholder for requesting a game
+                    } else if text.starts_with("/leave_lobby") {
+                        // Placeholder for leaving the lobby gracefully
+                    } else if text.starts_with("/join_lobby") {
+                        handle_join_lobby(text, &mut *client_ws_tx.lock().await, &lobby).await;
                     }
                 }
-                Err(e) => {
-                    warn!("Error receiving message: {}", e);
-                    break;
-                }
             }
-        }
-
-
-
-    // Log disconnection and remove client from the shared state
-    info!("Client {} disconnected", client_id);
-    clients.lock().await.remove(&client_id);
-
-
-    // Spawn a task to write messages to the WebSocket
-    let sender_task = spawn(async move {
-        while let Ok(message) = client_tx.subscribe().recv().await {
-            if let Err(e) = client_ws_tx.send(Message::text(message)).await {
-                error!("Failed to send message: {}", e);
+            Err(e) => {
+                warn!("Error receiving message: {}", e);
                 break;
             }
         }
-    });
 
-    // Wait for the sender task to complete
-    let _ = sender_task.await;
+        if !running.load(Ordering::Relaxed) {
+            break;
+        }
+    }
+
+    info!("Client {} disconnected", client_id);
+    clients.lock().await.remove(&client_id);
+}
+
+async fn handle_send(
+    client_id: Uuid,
+    clients: Arc<Mutex<HashMap<Uuid, broadcast::Sender<String>>>>,
+) {
+    let mut broadcast_receiver = clients.lock().await[&client_id].subscribe();
+    while let Ok(message) = broadcast_receiver.recv().await {
+        if let Some(client_ws_tx) = clients.lock().await.get_mut(&client_id) {
+            if let Err(e) = client_ws_tx.send(message) {
+                error!("Failed to send message: {}", e);
+                break;
+            }            
+        }
+    }
+}
+
+async fn handle_join_lobby(
+    message: &str,
+    client_ws_tx: &mut SplitSink<WebSocket, Message>,
+    lobby: &Arc<Mutex<Lobby>>,
+) {
+    let player_name = message.trim_start_matches("/join_lobby").trim();
+    let player_id = Uuid::new_v4();
+    let player = Player {
+        id: player_id,
+        name: player_name.to_string(),
+    };
+    lobby.lock().await.join_lobby(player);
+
+    let join_message = format!("You have joined the lobby as '{}'", player_name);
+    let _ = client_ws_tx.send(Message::text(join_message)).await;
+}
+
+async fn cleanup_connection(
+    client_id: Uuid,
+    clients: Arc<Mutex<HashMap<Uuid, broadcast::Sender<String>>>>,
+    running: Arc<AtomicBool>,
+) {
+    info!("Client {} disconnected", client_id);
+    clients.lock().await.remove(&client_id);
+
+    if clients.lock().await.is_empty() {
+        running.store(false, Ordering::Relaxed);
+    }
 }
